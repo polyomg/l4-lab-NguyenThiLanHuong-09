59fedcb76bd35f6eb69edd00e5deb85d723d90ba
NguyenThiLanHuong-09 <huongntlts00949@fpt.edu.vn>
2025-10-19 15:13:26 +0700

    Lab4_TS00949_NguyenThiLanHuong

--- Lab4/src/main/java/poly/edu/Lab4Application.java
+++ Lab4/src/main/java/poly/edu/Lab4Application.java
@@ -1,4 +1,4 @@
-package com.poly.lab4;
+package poly.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Lab4/src/main/java/poly/edu/config/MessageConfig.java
@@ -0,0 +1,4 @@
+package poly.edu.config;
+
+public class MessageConfig {
+}
